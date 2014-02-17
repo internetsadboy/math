bdb1f5d5875f67d5970ada0997939ee8cf338b20
jared <jaredhalpert90@gmail.com>
2014-02-17 00:27:04 -0800

    added binary search implementations

---
+++ algorithms/binary-search/js/bs-iterative.js
@@ -0,0 +1,24 @@
+/*
+ * @author ghostsnstuff
+ * params: sorted list and value to be searched for
+ * while loop = search behavior
+ * break loop if value found or if startIndex = stopIndex
+ * if value is < current middle -> reduce the list search content to the lower half 
+ * if value is > current middle -> reduce the list search content to the upper half
+ * update middle after if else if
+ * return middle if value == list[middle] else return -1
+ */
+
+module.exports = function(array, value) {  
+  var startIndex = 0,
+      stopIndex = array.length - 1,
+      middle = Math.floor((startIndex + stopIndex)/2);      
+  while(array[middle] !== value && startIndex < stopIndex) {
+    if(value < array[middle]) stopIndex = middle - 1;
+    else if(value > array[middle]) startIndex = middle + 1;
+    middle = Math.floor((startIndex + stopIndex)/2);
+  }
+  return (array[middle] === value) ? middle : -1;
+}
+
+
