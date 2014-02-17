18c9086872b65f9c5ec007e50e1a86d15147ce96
ghostsnstuff <jaredhalpert90@gmail.com>
2014-02-16 23:52:17 -0800

    Update stats.js

--- stats.js
+++ stats.js
@@ -6,10 +6,10 @@ exports.mean = function(data, decimals) {
 }
 
 exports.median = function(data) {
-	var median = 0;
-	data = data.sort(function (a, b) { return a - b });
-	if(data.length % 2 === 0) return (data[(data.length/2)-1] + data[(data.length/2)])/2;
-	else return data[~~(data.length/2)];
+   var median = 0;
+   data = data.sort(function (a, b) { return a - b });
+   if(data.length % 2 === 0) return (data[(data.length/2)-1] + data[(data.length/2)])/2;
+   else return data[~~(data.length/2)];
 }
 
 exports.mode = function(data) {
