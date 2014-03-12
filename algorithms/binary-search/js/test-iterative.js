9278871f2c7bfbd1fbfd1b5f628823605063a886
ghostsnstuff <jaredhalpert90@gmail.com>
2014-03-12 10:31:17 -0700

    Update test-iterative.js

--- algorithms/binary-search/js/test-iterative.js
+++ algorithms/binary-search/js/test-iterative.js
@@ -1,4 +1,4 @@
-var BinarySearch = require('./bs-iterative.js')
+var BinarySearch = require('./bs-iterative')
   , data = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
   , value = 4;
 console.log(data);
