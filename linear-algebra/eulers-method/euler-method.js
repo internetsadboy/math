a6660fda8c321fb4e0c53ce77291c2d23c0ce7f9
jared <jaredhalpert90@gmail.com>
2014-04-30 22:45:22 -0700

    euley py

---
+++ linear-algebra/eulers-method/euler-method.js
@@ -0,0 +1,29 @@
+// Euler's Method
+// http://en.wikipedia.org/wiki/Euler_method
+// user input: n, d, x, and poly 
+// poly: dx/dt == f(t,x)
+// n: # of steps 
+// d: domain a <= t <= b
+// x: initial value 
+// h: step size
+// t: the delta of current steps taken
+
+var n = 4
+var d = [0,2];
+var x = 0.5;
+var h = (d[0]+d[1])/n; 
+var t = 0;
+
+function poly(x,t) {
+	return x - Math.pow(t,2) + 1;
+}
+
+function EulersMethod() {
+	for(var k = 0; k < n; k++) {
+		t = k*h;
+	 	x = h*poly(x,t)+x;
+	} 
+	return x;
+}
+
+EulersMethod(); // 4.4375
